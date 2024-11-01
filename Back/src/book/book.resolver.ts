5049e00a61094b17d20d58c83f0e03daad767255
taewoo <rlaxodn322@gmail.com>
2024-11-01 16:32:57 +0900

    GraphQL FILTER 기능 추가

--- Back/src/book/book.resolver.ts
+++ Back/src/book/book.resolver.ts
@@ -1,6 +1,7 @@
 import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
 import { Book } from './book.entity';
 import { BookService } from './book.service';
+import { In } from 'typeorm';
 
 @Resolver(() => Book)
 export class BookResolver {
@@ -20,5 +21,28 @@ export class BookResolver {
   @Query(() => Book)
   async book(@Args('id', { type: () => Int }) id: number): Promise<Book> {
     return this.bookService.findOne(id);
+  }
+
+  @Mutation(() => Book)
+  async updateBook(
+    @Args('id', { type: () => Int }) id: number,
+    @Args('title', { nullable: true }) title?: string,
+    @Args('author', { nullable: true }) author?: string,
+  ): Promise<Book> {
+    return this.bookService.update(id, { title, author });
+  }
+
+  @Mutation(() => Boolean)
+  async deleteBook(
+    @Args('id', { type: () => Int }) id: number,
+  ): Promise<boolean> {
+    return this.bookService.delete(id);
+  }
+  @Query(() => [Book])
+  async filterBooks(
+    @Args('title', { nullable: true }) title: string,
+    @Args('author', { nullable: true }) author: string,
+  ): Promise<Book[]> {
+    return this.bookService.filter({ title, author });
   }
 }
--- Back/src/book/book.service.ts
+++ Back/src/book/book.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Book } from './book.entity';
 import { Repository } from 'typeorm';
@@ -19,5 +19,33 @@ export class BookService {
   async create(data: { title: string; author: string }): Promise<Book> {
     const book = this.bookrepository.create(data);
     return this.bookrepository.save(book);
+  }
+  async update(
+    id: number,
+    data: Partial<{ title: string; author: string }>,
+  ): Promise<Book> {
+    const book = await this.bookrepository.findOne({ where: { id } });
+    if (!book) throw new NotFoundException(`Book with ID ${id} not found`);
+    Object.assign(book, data);
+    return this.bookrepository.save(book);
+  }
+  async delete(id: number): Promise<boolean> {
+    const result = await this.bookrepository.delete(id);
+    return result.affected > 0;
+  }
+
+  async filter(filterCriteria: {
+    title?: string;
+    author?: string;
+  }): Promise<Book[]> {
+    const { title, author } = filterCriteria;
+    const queryBuilder = this.bookrepository.createQueryBuilder('book');
+    if (title)
+      queryBuilder.andWhere('book.title Lke: title', { title: `%${title}%` });
+    if (author)
+      queryBuilder.andWhere('book.author Like: author', {
+        author: `%${author}%`,
+      });
+    return queryBuilder.getMany();
   }
 }
